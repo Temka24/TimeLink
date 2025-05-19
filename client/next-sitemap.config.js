883adfef852f15d25d6d5b7a39e560891048554e
Temka <thxdeadshotxht@gmail.com>
2025-05-19 10:14:10 +0800

    fix next-sitemsp

--- client/next-sitemap.config.js
+++ client/next-sitemap.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://timelink.mn',
+    siteUrl: 'https://timelink.mn', // өөрийн домэйн
     generateRobotsTxt: true,
-    changefreq: 'weekly',
-    priority: 0.7,
-    sitemapSize: 5000,
+    exclude: ['/404'],
+    transform: async (config, path) => {
+        return {
+            loc: path,
+            changefreq: 'weekly',
+            priority: 0.7,
+            lastmod: new Date().toISOString(),
+            alternateRefs: [],
+        };
+    },
 };

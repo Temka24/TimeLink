885fcfee4d73c28bcda234f8bbf8fbdce90f57bc
Temka <thxdeadshotxht@gmail.com>
2025-05-22 23:59:53 +0800

    evening

--- client/src/app/font.ts
+++ client/src/app/font.ts
@@ -3,5 +3,5 @@ import { Outfit } from 'next/font/google';
 export const outfit = Outfit({
     subsets: ['latin'],
     display: 'swap',
-    weight: ['400', '500','600', '700'],
+    weight: ['400', '500','600', '700', '800', '900'],
 });
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+
 export const metadata = {
     title: 'Цаг товлолт, захиалгын платформ | TimeLink',
     description: 'TimeLink нь хувь хүн болон бизнесүүдэд зориулсан цаг товлох систем юм.',
@@ -28,7 +31,28 @@ export const metadata = {
 export default function HomePage() {
     return (
         <>
-            <div>Okey thit is SSG</div>
+            <div className="w-[100vw] min-h-screen pt-[20px] overflow-hidden">
+                <div className="max-w-[1600px] mx-auto relative overflow-x-hidden shadow-lg rounded-3xl px-[40px] pb-[40px]">
+                    <nav className="flex items-center justify-center gap-[60vw] ">
+                        <section className="flex items-center justify-center gap-2">
+                            <Image src="/favicon.png" alt="logo" height={40} width={40} />
+                            <div className="text-[24px] font-semibold flex">
+                                <p className="text-[#914bf1]">Time</p>
+                                <p>Link</p>
+                            </div>
+                        </section>{' '}
+                        <Button>Нэвтрэх</Button>
+                    </nav>
+                    <h1 className="text-center text-[40px] font-black flex mt-[85px] leading-[40px] flex-col items-center justify-center gap-0">
+                        Уулзалт, цаг товлолтоо автоматжуулж
+                        <span className="text-[#914bf1]"> маш хялбараар шийд</span>
+                    </h1>
+                    <p className='mt-[30px] text-center max-w-[600px] mx-auto text-note '>
+                        Цаг товлолтоо илүү хурдан хийж, эцэс төгсгөлгүй имэйл бичхээс салаарай.
+                        Таны өмнөөс бүх асуудлыг шийдэх болно
+                    </p>
+                </div>
+            </div>
         </>
     );
 }
